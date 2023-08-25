57b1127b3807d8ee7d66740ad8b61cbd4d22d337
sabarirajan <138021727+SA-L-svg@users.noreply.github.com>
2023-08-25 14:04:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,94 @@
+# -------------------------------------------------------------------------------------
+#  impot query
+from flask import Flask, jsonify, request
+import mysql.connector
+import json
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField
+# from wtforms.validators import InputRequired, Email, Length, Regexp
+import re
+# import email_validator
+# -----------------------------------------------------------------------------------------------------
+# write a code
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+# Password validation regular expression
+PASSWORD_REGEX = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+
+Email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+def validate_password(password):
+    return re.match(PASSWORD_REGEX, password)
+
+def validate_email(email):
+    return re.match(Email_regex,email)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+   
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    
+    if not validate_email(email):
+        return jsonify({'message': 'Invalid email format'})
+
+    # Perform email and password validation
+    
+    if not validate_password(password):
+        return jsonify({'message': 'Invalid password format'})
+    
+    # If validation succeeds, store the values in the database
+    mydb = mysql.connector.connect( host="localhost",port=3306, user="root" , password='', database="api_validation")
+      # Use the 'connection' attribute
+    cursor = mydb.cursor()
+    query = "INSERT INTO apiuser (email, password) VALUES (%s, %s)"
+    cursor.execute(query, (email, password))
+    mydb.commit()
+    cursor.close()
+    return jsonify({'message': 'Login successful and user data stored in the database'})
+
+@app.route('/apiuser', methods=['GET'])
+def getdata():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="root",  # Provide the correct username
+            password="",  # Provide the correct password
+            database="api_validation"
+        )
+        
+        # Create a cursor
+        cursor = mydb.cursor()
+
+        # Execute a SQL query
+        query = "SELECT * FROM apiuser;"
+        cursor.execute(query)
+
+        # Fetch the results
+        result = cursor.fetchall()
+
+        # Close the cursor and connection
+        cursor.close()
+        mydb.close()
+
+        # Process the result and return a response
+        # (You should format the result as needed)
+        response_data = [{"id": row[0], "name": row[1],"password":row[2]} for row in result]
+        return jsonify(response_data)
+
+    except Exception as e:
+        return str(e), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True,port=8888)
+
